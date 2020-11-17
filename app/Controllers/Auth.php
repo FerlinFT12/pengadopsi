dd55ce2bb1a1380a471682610db1805ba477e570
DevidSetiawan <devid.setiawan94@gmail.com>
2020-11-17 14:15:19 +0700

    merubah isi form validation

--- app/Controllers/Auth.php
+++ app/Controllers/Auth.php
@@ -21,15 +21,29 @@ class Auth extends BaseController
   public function save_register()
   {
     if ($this->validate([
-      'nama_user' => [
-        'label' => 'Nama User',
+      'nm_dpn' => [
+        'label' => 'Nama Depan',
+        'rules' => 'required',
+        'errors' => [
+          'required' => '{field}  Wajib untuk diisi!'
+        ]
+      ],
+      'nm_blkg' => [
+        'label' => 'Nama Belakang',
         'rules' => 'required',
         'errors' => [
           'required' => '{field}  Wajib untuk diisi!'
         ]
       ],
       'email' => [
-        'label' => 'Email',
+        'label' => 'Alamat Email',
+        'rules' => 'required',
+        'errors' => [
+          'required' => '{field}  Wajib untuk diisi!'
+        ]
+      ],
+      'institusi' => [
+        'label' => 'Institusi / Perorang',
         'rules' => 'required',
         'errors' => [
           'required' => '{field}  Wajib untuk diisi!'
@@ -42,29 +56,102 @@ class Auth extends BaseController
           'required' => '{field} Wajib untuk diisi!'
         ]
       ],
+      'no_wa' => [
+        'label' => 'No Whatsapp',
+        'rules' => 'required',
+        'errors' => [
+          'required' => '{field} Wajib untuk diisi!'
+        ]
+      ],
+      'tempat_lahir' => [
+        'label' => 'Tempat Lahir',
+        'rules' => 'required',
+        'errors' => [
+          'required' => '{field} Wajib untuk diisi!'
+        ]
+      ],
+      'tgl_lahir' => [
+        'label' => 'tgl_lahir',
+        'rules' => 'required',
+        'errors' => [
+          'required' => '{field} Wajib untuk diisi!'
+        ]
+      ],
+      'provinsi' => [
+        'label' => 'Provinsi',
+        'rules' => 'required',
+        'errors' => [
+          'required' => '{field} Wajib untuk diisi!'
+        ]
+      ],
+      'kab_kota' => [
+        'label' => 'Kabupaten Kota',
+        'rules' => 'required',
+        'errors' => [
+          'required' => '{field} Wajib untuk diisi!'
+        ]
+      ],
+      'kec' => [
+        'label' => 'Kecamatan',
+        'rules' => 'required',
+        'errors' => [
+          'required' => '{field} Wajib untuk diisi!'
+        ]
+      ],
+      'kel' => [
+        'label' => 'Kelurahan',
+        'rules' => 'required',
+        'errors' => [
+          'required' => '{field} Wajib untuk diisi!'
+        ]
+      ],
+      'nm_jl' => [
+        'label' => 'Nama Jalan',
+        'rules' => 'required',
+        'errors' => [
+          'required' => '{field} Wajib untuk diisi!'
+        ]
+      ],
+      'nama_user' => [
+        'label' => 'Username',
+        'rules' => 'required',
+        'errors' => [
+          'required' => '{field} Wajib untuk diisi!'
+        ]
+      ],
       'password' => [
         'label' => 'Password',
         'rules' => 'required',
         'errors' => [
           'required' => '{field} Wajib untuk diisi!'
         ]
-      ],
-      'repassword' => [
-        'label' => 'Ketik Ulang Password',
-        'rules' => 'required|matches[password]',
-        'errors' => [
-          'required' => '{field} Wajib untuk diisi!',
-          'matches' => '{field} Anda Tidak Sama!'
-        ]
       ]
     ])) {
       // jika valid
       $data = array(
-        'nama_user' => $this->request->getPost('nama_user'),
+        'nm_dpn' => $this->request->getPost('nm_dpn'),
+        'nm_blkg' => $this->request->getPost('nm_blkg'),
         'email' => $this->request->getPost('email'),
+        'institusi' => $this->request->getPost('institusi'),
         'no_hp' => $this->request->getPost('no_hp'),
+        'no_wa' => $this->request->getPost('no_wa'),
+        'tempat_lahir' => $this->request->getPost('tempat_lahir'),
+        'tgl_lahir' => $this->request->getPost('tgl_lahir'),
+        'provinsi' => $this->request->getPost('provinsi'),
+        'kab_kota' => $this->request->getPost('kab_kota'),
+        'kec' => $this->request->getPost('kec'),
+        'kel' => $this->request->getPost('kel'),
+        'nm_jl' => $this->request->getPost('nm_jl'),
+        'level' => $this->request->getPost('level'),
+        'bantuan' => $this->request->getPost('bantuan'),
+        'paket' => $this->request->getPost('paket'),
+        'harga' => $this->request->getPost('harga'),
+        'jml' => $this->request->getPost('jml'),
+        'total' => $this->request->getPost('total'),
+        'bukti' => $this->request->getPost('bukti'),
+        'catatan' => $this->request->getPost('catatan'),
+        'nama_user' => $this->request->getPost('nama_user'),
         'password' => $this->request->getPost('password'),
-        'level' =>  3
       );
       $this->ModelAuth->save_register($data);
       session()->setFlashdata('pesan', 'Registrasi Berhasil!');
